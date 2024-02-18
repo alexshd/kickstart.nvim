654a7378b81785a79641b3c11fa0272b3bb09be4
Alex Shadrin <alex@shdlabs.com>
2024-02-18 12:02:49 +0200

    no comments needed

--- lua/custom/plugins/surround.lua
+++ lua/custom/plugins/surround.lua
@@ -1,11 +1,9 @@
 return {
   'kylechui/nvim-surround',
   dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-  version = '*', -- Use for stability; omit to use "main" branch for the latest features
+  version = '*',
   event = 'VeryLazy',
   config = function()
-    require('nvim-surround').setup {
-      -- Configuration here, or leave empty to use defaults
-    }
+    require('nvim-surround').setup {}
   end,
 }
